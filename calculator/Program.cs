30f2c11dc70e0ae8eff1622e783365b36a688064
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-05-23 09:38:00 -0500

    metodos

---
+++ calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿int Suma(int a, int b)
+{
+  Console.WriteLine(a + b);
+  return a + b;
+}
+
+int Resta(int a, int b)
+{
+  Console.WriteLine(a - b);
+  return a - b;
+}
+
+int Multiplicacion(int a, int b)
+{
+  Console.WriteLine(a * b);
+  return a * b;
+}
+
+int Division(int a, int b)
+{
+  if (b == 0)
+  {
+    Console.WriteLine("Error: Division by zero is not allowed.");
+    return 0;
+  }
+  else
+  {
+    Console.WriteLine(a / b);
+    return a / b;
+  }
+}
+
+int resultadoSuma = Suma(5, 3);
+int resultadoResta = Resta(5, 3);
+int resultadoMultiplicacion = Multiplicacion(5, 3);
+int resultadoDivision = Division(5, 0);
+int resultadoDivision2 = Division(5, 2);
