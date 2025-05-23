eef75b338dedf031d11af21f2cc41343aa38a3a9
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-05-23 08:51:52 -0500

    firts class .net and c#

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+var nombres = "Ortega Ivan";
+
+var edad = 25;
+int edad2 = 40;
+
+double precio = 10.5;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(nombres);
+Console.WriteLine(edad);
+Console.WriteLine(edad2);
+Console.WriteLine(precio);
+
+const string nombreConst = "Especificacion";
+Console.WriteLine(nombreConst);
