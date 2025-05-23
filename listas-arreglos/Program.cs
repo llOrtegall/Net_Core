8f216407e05647f2303d531b908d09eacc2ac0e1
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-05-23 09:27:25 -0500

    class to list and array, example to switchs

---
+++ listas-arreglos/Program.cs
@@ -0,0 +1,41 @@
+﻿string nombre1 = "Pepito";
+string nombre2 = "Juanito";
+
+string[] nombres = new string[2];
+nombres[0] = nombre1;
+nombres[1] = nombre2;
+
+Console.WriteLine("Los nombres son:");
+for (int i = 0; i < nombres.Length; i++)
+{
+  Console.WriteLine(nombres[i]);
+}
+
+List<string> listaNombres = new List<string>();
+listaNombres.Add("Pepito");
+listaNombres.Add("Juanito");
+listaNombres.Add("Maria");
+
+Console.WriteLine("Los nombres en la lista son:");
+foreach (string nombre in listaNombres)
+{
+  Console.WriteLine(nombre);
+}
+
+// example for switch case
+
+switch (listaNombres[2])
+{
+  case "Pepito":
+    Console.WriteLine("El nombre es Pepito");
+    break;
+  case "Juanito":
+    Console.WriteLine("El nombre es Juanito");
+    break;
+  case "Maria":
+    Console.WriteLine("El nombre es Maria");
+    break;
+  default:
+    Console.WriteLine("El nombre no es Pepito ni Juanito");
+    break;
+}
