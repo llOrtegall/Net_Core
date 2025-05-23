28d53aaf718daba32185885fcad8cc8f1c8d9398
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-05-23 09:16:40 -0500

    Add initial implementation of ciclos with for, while, and foreach examples

---
+++ ciclos/Program.cs
@@ -0,0 +1,27 @@
+﻿int contador = 10;
+
+for (int i = 1; i <= contador; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("------------------------------------");
+
+// Example de un ciclo while
+int contador2 = 10;
+while (contador2 > 0)
+{
+    Console.WriteLine(contador2);
+    contador2--;
+}
+
+Console.WriteLine("------------------------------------");
+
+// Example de un ciclo do forEach
+Console.WriteLine("Por favor ingresa tu nombre: ");
+string nombre = Console.ReadLine();
+
+foreach (char letra in nombre)
+{
+    Console.WriteLine(letra);
+}
