738288a622c00ee1597f08eac53ea1587c908b2f
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-05-23 08:30:08 -0500

    delete files

--- src/connections/metas.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2/promise'
-
-export const pool = mysql.createPool({
-  host: process.env.METAS_HOST,
-  user: process.env.METAS_USER,
-  password: process.env.METAS_PASS,
-  port: process.env.METAS_PORT,
-  database: process.env.METAS_DATABASE
-})
--- src/routes/clienteFiel.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { createdClientFiel, getClientFiel } from '../controllers/ClienteFiel.Controllers.js'
-
-export const ClienteFiel = Router()
-
-ClienteFiel.post('/getCF', getClientFiel)
-
-ClienteFiel.post('/newCF', createdClientFiel)
