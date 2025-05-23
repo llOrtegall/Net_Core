92b089edb62f772e7d2e40fc1b41488a839994d0
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-05-23 08:58:07 -0500

    condicionales

---
+++ condiciones/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int edad = 18;
+
+Console.WriteLine("Ingrese su edad:");
+// Convertir la entrada a un número entero
+edad = Convert.ToInt32(Console.ReadLine());
+
+if (edad >= 18)
+    Console.WriteLine("Eres mayor de edad");
+else
+    Console.WriteLine("Eres menor de edad");
+    
+// Operador ternario
+string resultado = (edad >= 18) ? "Eres mayor de edad" : "Eres menor de edad";
+Console.WriteLine(resultado);
+
